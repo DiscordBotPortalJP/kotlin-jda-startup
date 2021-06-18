13347c32978c6038ee65dbe2955e76d082ae5fb8
hisuie08 <keisyu0825@gmail.com>
2021-06-18 21:50:29 +0900

    commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,4 @@
 import net.dv8tion.jda.api.*
-import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.requests.GatewayIntent
@@ -14,12 +13,7 @@ class BotClient : ListenerAdapter(){
             .build()
     }
     
-    override fun onReady(event : ReadyEvent) {
-        println("ready!")
-    }
-    
     override fun onGuildMessageReceived(event : GuildMessageReceivedEvent) {
-        println("message received")
         if(event.message.contentDisplay == "/ping"){
             event.channel.sendMessageFormat("pong").queue()
         }
