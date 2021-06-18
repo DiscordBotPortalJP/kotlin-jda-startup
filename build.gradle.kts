0ab89680d2771923edd8d3485dd8079bd3725174
hisuie08 <keisyu0825@gmail.com>
2021-06-18 17:35:13 +0900

    update gradle wrapper version add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.10"
+    application
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "org.example"
@@ -7,8 +9,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://m2.dv8tion.net/releases")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("net.dv8tion:JDA:4.3.0_277")
+}
+
+application{
+    mainClass.set("MainKt")
 }
